c8ceb0ed24c71af884344202b912952dcccdb63f
Jethro Fredericks <fredericks22.jethro@gmail.com>
2017-10-30 15:31:30 +0200

    Sorcery authentication complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 include CommentsHelper
 
 class CommentsController < ApplicationController
+    before_action :require_login, except: [:create]
+
     def create
         @comment = Comment.new(comment_params)
         @comment.article_id = params[:article_id]
